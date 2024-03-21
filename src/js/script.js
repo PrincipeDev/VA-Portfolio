c7086cfef31a09b9396115089934d59c1091dd9b
José Borges Costa <borgesdevfront@gmail.com>
2024-03-20 22:39:33 -0300

    estruturação finalizada

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+const containerProjeto = document.getElementsByClassName(".container-projet")
+const prevBtn = document.getElementsByClassName(".voltar")
+const nextBtn = document.getElementsByClassName(".proximo")
+const cards = document.getElementsById("#cards")
+const card = document.getElementsByClassName(".card")
+const contador = document.getElementsByClassName(".count")
+
+slide = [
+  {
+    "titulo": "Escritorio",
+    "image": "../img/1-Escritorio2.png"
+  },
+  {
+    "titulo": "Escritorio 2",
+    "image": "../img/2-Escritorio2.png"
+  },
+  {
+    "titulo": "Suite",
+    "image": "../img/3-Suite.png"
+  },
+  {
+    "titulo": "Suite 1",
+    "image": "../img/4-Suite.png"
+  },
+  {
+    "titulo": "Suite 2",
+    "image": "../img/5-Suite.png"
+  }
+]
