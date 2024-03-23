fe11df0ba16374153e81b50c96b9d558c1216f41
José Borges Costa <borgesdevfront@gmail.com>
2024-03-22 21:10:15 -0300

    novo projeto adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -4,33 +4,38 @@ const nextBtn = document.querySelector(".proximo")
 const cards = document.getElementById("cards")
 let contador = document.querySelector(".count")
 
-contador = 0
+contador = 1
 
 let slide = [
   {
     "titulo": "Escritorio",
     "image": "/src/img/Escritorio.png",
-    "index": 0
+    "index": 1
   },
   {
     "titulo": "Escritorio 2",
     "image": "/src/img/Escritorio2.png",
-    "index": 0
+    "index": 1
   },
   {
     "titulo": "Suite",
     "image": "/src/img/Suite.png",
-    "index": 1
+    "index": 2
   },
   {
     "titulo": "Suite 1",
     "image": "/src/img/Suite2.png",
-    "index": 1
+    "index": 2
   },
   {
     "titulo": "Suite 2",
     "image": "/src/img/Suite3.png",
-    "index": 2
+    "index": 3
+  },
+  {
+    "titulo": "Sala",
+    "image": "/src/img/sala.jpg",
+    "index": 3
   }
 ]
 
@@ -54,8 +59,8 @@ function updateProjetos() {
 updateProjetos()
 
 function nextSlide() {
-  if(contador > (slide.length % 2)) {
-    contador = 0
+  if(contador == (slide.length / 2)) {
+    contador = 1
   } else {
     contador++
   }
@@ -63,7 +68,7 @@ function nextSlide() {
 }
 
 function prevSlide() {
-  if(contador > 0) {
+  if(contador > 1) {
     contador--
   } else {
     contador = Math.floor(slide.length / 2)
