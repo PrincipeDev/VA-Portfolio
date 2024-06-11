ed42719a14f0b29d7533ce444cbcc268995ffbe6
Borges Costta <borgesdevfront@gmail.com>
2024-06-11 17:06:01 -0300

    Atualizações necessarias.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
-const prevBtn = document.querySelector(".voltar")
-const nextBtn = document.querySelector(".proximo")
-const cards = document.getElementById("cards")
-const btnWhats = document.getElementById("btn-whats")
-let contador = document.querySelector(".count")
+const prevBtn = document.querySelector(".voltar");
+const nextBtn = document.querySelector(".proximo");
+const cards = document.getElementById("cards");
+const btnWhats = document.getElementById("btn-whats");
+let contador = 0; // Inicie o contador em 0
+let resizeTimer;
 
 let slide = [
   {
@@ -50,10 +51,7 @@ let slide = [
     "image": "/src/img/Quarto.png",
     "index": 8
   }
-]
-
-contador = 0 // Inicie o contador em 0
-let resizeTimer;
+];
 
 function atualizarTamanhoDaTela() {
   clearTimeout(resizeTimer);
@@ -61,13 +59,11 @@ function atualizarTamanhoDaTela() {
     const larguraDaTela = window.innerWidth;
 
     if (larguraDaTela > 800) {
-      // Chama a função para larguras de tela maiores que 800
       paraLarguraMaiorQue800();
     } else {
-      // Chama a função para larguras de tela menores ou iguais a 800
       paraLarguraMenorIgualA800();
     }
-  }, 250); // Defina um tempo de espera adequado, por exemplo, 250ms
+  }, 250);
 }
 
 atualizarTamanhoDaTela();
@@ -75,92 +71,89 @@ window.addEventListener('resize', atualizarTamanhoDaTela);
 
 function paraLarguraMaiorQue800() {
   function updateProjetos() {
-    cards.innerHTML = ""
-  
+    cards.innerHTML = "";
+
     if (slide.length % 2 === 0 || contador < slide.length - 1) {
-      // Renderiza dois elementos se o número de imagens for par ou se não for o último índice
       for (let i = contador; i < contador + 2 && i < slide.length; i++) {
-        const item = slide[i] // Use o índice diretamente
-        const cardElement = document.createElement("div")
-        cardElement.classList.add("card")
-  
+        const item = slide[i];
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+
         cardElement.innerHTML = `
           <img src="${item.image}"/>
           <h2>${item.titulo}</h2>
-        `
-        cards.appendChild(cardElement)
+        `;
+        cards.appendChild(cardElement);
       }
     } else {
-      // Renderiza a última imagem sozinha se o número de imagens for ímpar e for o último índice
-      const item = slide[contador]
-      const cardElement = document.createElement("div")
-      cardElement.classList.add("card")
-  
+      const item = slide[contador];
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+
       cardElement.innerHTML = `
         <img src="${item.image}"/>
         <h2>${item.titulo}</h2>
-      `
-      cards.appendChild(cardElement)
+      `;
+      cards.appendChild(cardElement);
     }
   }
-  
+
   updateProjetos();
-  
-  function nextSlide() {
-    contador += 2 // Avança de duas em duas imagens
-  
-    if (contador >= slide.length) {
-      contador = 0 // Garante que o contador não ultrapasse o último índice
-    }
-    updateProjetos()
-  }
-  
-  function prevSlide() {
-    if(contador > 0) {
-      contador -= 2
-    } else {
-      contador = slide.length -2
-    }
-    updateProjetos()
-  }
-} 
+}
 
 function paraLarguraMenorIgualA800() {
-  function updateProjetos() { // Função utilizada para criar o elemento html contendo o carrossel de imagens
-    cards.innerHTML = ""
-  
-    slide.forEach(item => { // Percorre toda a lista para fazer a renderização correta de cada elemento
-      const cardElement = document.createElement("div")
-      cardElement.classList.add("card")
-  
+  function updateProjetos() {
+    cards.innerHTML = "";
+
+    slide.forEach(item => {
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+
       if (item.index === slide[contador].index) {
         cardElement.innerHTML = `
           <img src="${item.image}"/>
           <h2>${item.titulo}</h2>
-        `
-        cards.appendChild(cardElement)
+        `;
+        cards.appendChild(cardElement);
       }
-    })
+    });
   }
-  
-  updateProjetos()
-  
-  function nextSlide() {
-    contador = (contador + 1) % slide.length; // Avança o contador de forma circular
-    updateProjetos()
-  }
-  
-  function prevSlide() {
-    contador = (contador - 1 + slide.length) % slide.length; // Retrocede o contador de forma circular
-    updateProjetos()
-  }
+
+  updateProjetos();
 }
 
-prevBtn.addEventListener("click", prevSlide)
-nextBtn.addEventListener("click", nextSlide)
+function nextSlide() {
+  const larguraDaTela = window.innerWidth;
+  if (larguraDaTela > 800) {
+    contador += 2;
+    if (contador >= slide.length) {
+      contador = 0;
+    }
+  } else {
+    contador = (contador + 1) % slide.length;
+  }
+  atualizarTamanhoDaTela();
+}
+
+function prevSlide() {
+  const larguraDaTela = window.innerWidth;
+  if (larguraDaTela > 800) {
+    if (contador > 0) {
+      contador -= 2;
+    } else {
+      contador = slide.length - 2;
+    }
+  } else {
+    contador = (contador - 1 + slide.length) % slide.length;
+  }
+  atualizarTamanhoDaTela();
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
 
 btnWhats.addEventListener("click", function() {
-  const message = "Olá, Estou precisando conversar com você!"
-  const phone = "63992850579"
-  window.open(`https://wa.me/${phone}?text=${message}`, "_blank")
-})
+  const message = "Olá, Estou precisando conversar com você!";
+  const phone = "63992850579";
+  window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+});
